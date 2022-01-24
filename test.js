c5f2a6c27847d08cfad80ca9cadb61aa56345e9a
ouaksel <ouakselboukhalfa.ob@gmail.com>
2022-01-24 23:05:33 +0100

    commit_2

--- test.js
+++ test.js
@@ -1,13 +1,25 @@
 
-// on peut également déclarer et initialiser un objet de la façon suivante :
-
-let aPerson= new Object();
-aPerson.name = 'Jean';
-aPerson.age = 35;
-aPerson.gender = 'masculin';
-aPerson.interest = ['musique', 'badminton'];
-
-
-aPerson.sayHello = function() {console.log('Bonjour ! Je suis ' + this.name + '.'); };
-
-aPerson.sayHello();
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Dimanche";
+    break;
+  case 1:
+    day = "Lundi";
+    break;
+  case 2:
+     day = "Mardi";
+    break;
+  case 3:
+    day = "Mercredi";
+    break;
+  case 4:
+    day = "Jeudi";
+    break;
+  case 5:
+    day = "Vendredi";
+    break;
+  case 6:
+    day = "Samedi";
+}
+console.log("Aujourd'hui nous sommes " + day);
