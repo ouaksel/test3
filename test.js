c7a4d9609a4b667964f412e6faa35e02ca1babc6
ouaksel <ouakselboukhalfa.ob@gmail.com>
2022-01-24 22:45:16 +0100

    branche2

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
-const a = 12;
-let b = 45;
 
-b += a ;
-console.log("resultat de a + b = " + b);
+// on peut également déclarer et initialiser un objet de la façon suivante :
+
+let aPerson= new Object();
+aPerson.name = 'Jean';
+aPerson.age = 35;
+aPerson.gender = 'masculin';
+aPerson.interest = ['musique', 'badminton'];
+
+
+aPerson.sayHello = function() {console.log('Bonjour ! Je suis ' + this.name + '.'); };
+
+aPerson.sayHello();
